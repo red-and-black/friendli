840692d211f0f0085c632c2a708b6f45bdf841a0
Matthew Cengia <mattcen@mattcen.com>
2023-08-13 21:14:06 +1000

    Allow editing profile tags in admin
    I didn't test this code, but I'm 95% sure it's correct based on other
    Django projects I've used it in and the relevant documentation:
    https://docs.djangoproject.com/en/3.2/ref/contrib/admin/#modeladmin-objects

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from profiles.models import (
+    Language,
+    LookingFor,
+    PersonalInterest,
+    ProfessionalInterest,
+)
 
 # Register your models here.
+admin.site.register(Language)
+admin.site.register(LookingFor)
+admin.site.register(PersonalInterest)
+admin.site.register(ProfessionalInterest)
